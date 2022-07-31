3a8a9066f0ef17e93f8c01a7e9e79d022022f4ec
masumbillah360 <masumbillah6584@gmail.com>
2022-07-31 16:54:43 +0600

    added javascript file

---
+++ program.js
@@ -0,0 +1,7 @@
+const bangla = document.getElementById('bangla');
+const english = document.getElementById('english');
+const math = document.getElementById('math');
+const social = document.getElementById('social');
+const science = document.getElementById('science');
+const islam = document.getElementById('islam');
+const submitBtn = document.getElementById('submitBtn');
